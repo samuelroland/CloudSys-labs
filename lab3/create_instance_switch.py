16642ac2018d48afa506de6b2dd7a50f03da86d2
Olivia <olivia.manz@gmail.com>
2025-10-19 01:24:41 +0200

    add public ip to switch engine vm

--- lab3/create_instance_switch.py
+++ lab3/create_instance_switch.py
@@ -1,5 +1,8 @@
+import argparse
+import time
 from openstack import connection
 from openstack.config import OpenStackConfig
+from openstack.exceptions import NotFoundException
 
 IMAGE_NAME = "Ubuntu Jammy 22.04 (SWITCHengines)"
 FLAVOR_NAME = "m1.small"
@@ -33,13 +36,29 @@ def create_server(conn):
         key_name=keypair.name
     )
     server = conn.compute.wait_for_server(server)
+    print(f"VM '{SERVER_NAME}' créée avec succès.")
 
-    ip = None
-    for net, addresses in server.addresses.items():
-        ip = addresses[0]["addr"]
-        break
+    # find public network
+    public_net = None
+    for net in conn.network.networks():
+        if "public" in net.name.lower():
+            public_net = net
+            break
+    if public_net is None:
+        raise Exception("Impossible de trouver le réseau public pour la Floating IP.")
 
-    print(f"ssh -i {PRIVATE_KEYPAIR_FILE} ubuntu@{ip}")
+    floating_ip = conn.network.create_ip(floating_network_id=public_net.id)
+    
+    # Get VM port
+    ports = list(conn.network.ports(device_id=server.id))
+    if not ports:
+        raise Exception("Impossible de trouver le port réseau de la VM")
+    port_id = ports[0].id
+    
+    # Link Floating IP
+    conn.network.update_ip(floating_ip, port_id=port_id)
+
+    print(f"ssh -i {PRIVATE_KEYPAIR_FILE} ubuntu@{floating_ip.floating_ip_address}")
 
 def list_servers(conn):
     print("List Servers:")
@@ -57,6 +76,10 @@ def delete_server(conn, server_name):
 
 # --- Programme principal ---
 if __name__ == "__main__":
+    # parse arg
+    parser = argparse.ArgumentParser(description="Manage switch engine VM")
+    parser.add_argument("--delete-vm", type=str, help="VM id to delete")
+    args = parser.parse_args()
     # Load speciif config
     config = OpenStackConfig(config_files=["./switch/clouds.yaml"])
     cloud = config.get_one("engines")
@@ -64,7 +87,12 @@ if __name__ == "__main__":
     # Create connection
     conn = connection.Connection(config=cloud)
 
-    # list_images_dispo(conn)
-    create_server(conn)
+
     list_servers(conn)
-    # delete_server(conn, "1c5cccd0-6f48-432b-b359-7d8186f39832")
+    #list_images_dispo(conn)
+    
+    if args.delete_vm:
+        delete_server(conn, args.delete_vm)
+    else:
+         create_server(conn)
+   
